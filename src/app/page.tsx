337c5ac3e322cb8141401c14ce240e4d8c739fb7
ahm0xc <codingwebsa@gmail.com>
2025-03-06 00:57:28 +0600

    add: copy link functionality and improve navigation UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@
 
 import React from "react";
 
-import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  CheckIcon,
+  CopyIcon,
+} from "lucide-react";
 import useLocalStorage from "use-local-storage";
 
 import { Button } from "~/components/ui/button";
@@ -10,12 +15,10 @@ import { portfolioLinks } from "~/data/portfolios";
 
 export default function HomePage() {
   const [view, setView] = React.useState<"all">("all");
-  const [filteredPortfolioLinks, setFilteredPortfolioLinks] =
-    React.useState<typeof portfolioLinks>(portfolioLinks);
+  const [copiedLink, setCopiedLink] = React.useState<string | null>(null);
 
-  const [currentPortfolioIndex, setCurrentPortfolioIndex] = useLocalStorage<
-    number | null
-  >("current-portfolio-index", null);
+  const [currentPortfolioIndex, setCurrentPortfolioIndex] =
+    useLocalStorage<number>("current-portfolio-index", 0);
 
   function handleBack() {
     if (currentPortfolioIndex !== null && currentPortfolioIndex > 0) {
@@ -24,35 +27,26 @@ export default function HomePage() {
   }
 
   function handleForward() {
-    if (
-      currentPortfolioIndex !== null &&
-      currentPortfolioIndex < filteredPortfolioLinks.length - 1
-    ) {
+    if (currentPortfolioIndex < portfolioLinks.length - 1) {
       setCurrentPortfolioIndex(currentPortfolioIndex + 1);
     }
   }
 
-  React.useEffect(() => {
-    setFilteredPortfolioLinks(portfolioLinks);
+  function handleCopyLink() {
+    navigator.clipboard.writeText(portfolioLinks[currentPortfolioIndex] ?? "");
+    setCopiedLink(portfolioLinks[currentPortfolioIndex] ?? null);
 
-    if (filteredPortfolioLinks.length > 0) {
-      setCurrentPortfolioIndex(0);
-    } else {
-      setCurrentPortfolioIndex(null);
-    }
-  }, [view]);
+    setTimeout(() => {
+      setCopiedLink(null);
+    }, 2000);
+  }
 
-  React.useEffect(() => {
-    if (
-      currentPortfolioIndex !== null &&
-      currentPortfolioIndex >= filteredPortfolioLinks.length
-    ) {
-      setCurrentPortfolioIndex(filteredPortfolioLinks.length > 0 ? 0 : null);
-    }
-  }, [filteredPortfolioLinks, currentPortfolioIndex]);
+  const isCopied =
+    currentPortfolioIndex &&
+    copiedLink === portfolioLinks[currentPortfolioIndex];
 
   return (
-    <div className="h-screen flex flex-col gap-2">
+    <div className="h-screen flex flex-col">
       <header className="flex items-center justify-center pt-4 flex-col">
         <div className="flex items-center gap-2 w-full justify-center">
           <Button
@@ -61,11 +55,11 @@ export default function HomePage() {
             className="h-7 w-7 text-neutral-400"
             onClick={handleBack}
           >
-            <ArrowLeftIcon className="w-2 h-2" />
+            <ArrowLeftIcon className="!w-3" />
           </Button>
           <p className="text-neutral-400 text-sm">
             {currentPortfolioIndex !== null ? currentPortfolioIndex + 1 : 0} of{" "}
-            {filteredPortfolioLinks.length}
+            {portfolioLinks.length}
           </p>
           <Button
             size="icon"
@@ -73,15 +67,27 @@ export default function HomePage() {
             className="h-7 w-7 text-neutral-400"
             onClick={handleForward}
           >
-            <ArrowRightIcon className="w-2 h-2" />
+            <ArrowRightIcon className="!w-3" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-7 text-neutral-400 px-2"
+            onClick={handleCopyLink}
+          >
+            {isCopied ? (
+              <CheckIcon className="!w-3" />
+            ) : (
+              <CopyIcon className="!w-3" />
+            )}
+            Copy Link
           </Button>
         </div>
       </header>
-      <div className="w-full flex-1 p-4 pt-0">
+      <div className="w-full flex-1 p-4">
         <div className="border w-full h-full rounded-lg overflow-hidden">
           {currentPortfolioIndex !== null && (
             <iframe
-              src={filteredPortfolioLinks[currentPortfolioIndex]}
+              src={portfolioLinks[currentPortfolioIndex]}
               className="w-full h-full"
             />
           )}
